3f9e838eeecb569b17ba1bfc489974736af7b8ff
MLBSANS <nguyendangnhat0987@gmail.com>
2025-03-09 10:14:26 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,281 @@
+
+local G2L = {};
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["Name"] = [[5C7835335C7837305C7837395C7834335C7835305C783539]];
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(66, 66, 66);
+G2L["2"]["Size"] = UDim2.new(0.5288, 0, 0.48188, 0);
+G2L["2"]["Position"] = UDim2.new(0.23554, 0, 0.25906, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["2"]["Name"] = [[Main]];
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.TextLabel
+G2L["3"] = Instance.new("TextLabel", G2L["2"]);
+G2L["3"]["TextWrapped"] = true;
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["TextSize"] = 30;
+G2L["3"]["TextScaled"] = true;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/Michroma.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+G2L["3"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["3"]["BackgroundTransparency"] = 1;
+G2L["3"]["Size"] = UDim2.new(1, 0, 0.10769, 0);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["Text"] = [[SpyCPY]];
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.LocalScript
+G2L["4"] = Instance.new("LocalScript", G2L["2"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.TextBox
+G2L["5"] = Instance.new("TextBox", G2L["2"]);
+G2L["5"]["CursorPosition"] = -1;
+G2L["5"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["TextEditable"] = false;
+G2L["5"]["TextWrapped"] = true;
+G2L["5"]["TextSize"] = 30;
+G2L["5"]["TextDirection"] = Enum.TextDirection.RightToLeft;
+G2L["5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(41, 41, 41);
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/Fondamento.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["Size"] = UDim2.new(0.84144, 0, 0.68096, 0);
+G2L["5"]["Position"] = UDim2.new(0.01129, 0, 0.12669, 0);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Text"] = [[XIN LỖI HIỆN TẠI KHÔNG THẺ TỰ GHI ĐƯỢC!, BẠN CÓ THỂ SỬ DỤNG CÁC MODUL CÓ SẴN]];
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.infinite yield SS
+G2L["6"] = Instance.new("TextButton", G2L["2"]);
+G2L["6"]["TextWrapped"] = true;
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["TextSize"] = 14;
+G2L["6"]["TextScaled"] = true;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(101, 101, 101);
+G2L["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["6"]["Size"] = UDim2.new(0.12923, 0, 0.11402, 0);
+G2L["6"]["Name"] = [[infinite yield SS]];
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["Text"] = [[infinite yield SS]];
+G2L["6"]["Position"] = UDim2.new(0.86073, 0, 0.12669, 0);
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.infinite yield SS.LocalScript
+G2L["7"] = Instance.new("LocalScript", G2L["6"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Clear
+G2L["8"] = Instance.new("TextButton", G2L["2"]);
+G2L["8"]["TextWrapped"] = true;
+G2L["8"]["BorderSizePixel"] = 0;
+G2L["8"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["8"]["TextSize"] = 35;
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(91, 91, 91);
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["Size"] = UDim2.new(0.30819, 0, 0.13936, 0);
+G2L["8"]["Name"] = [[Clear]];
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[CLEAR]];
+G2L["8"]["Position"] = UDim2.new(0.54454, 0, 0.83299, 0);
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Clear.LocalScript
+G2L["9"] = Instance.new("LocalScript", G2L["8"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Executor
+G2L["a"] = Instance.new("TextButton", G2L["2"]);
+G2L["a"]["TextWrapped"] = true;
+G2L["a"]["BorderSizePixel"] = 0;
+G2L["a"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["a"]["TextSize"] = 35;
+G2L["a"]["BackgroundColor3"] = Color3.fromRGB(91, 91, 91);
+G2L["a"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["a"]["Size"] = UDim2.new(0.52196, 0, 0.13936, 0);
+G2L["a"]["Name"] = [[Executor]];
+G2L["a"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["a"]["Text"] = [[EXECUTOR]];
+G2L["a"]["Position"] = UDim2.new(0.01129, 0, 0.83299, 0);
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Executor.LocalScript
+G2L["b"] = Instance.new("LocalScript", G2L["a"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.ExSerNew
+G2L["c"] = Instance.new("TextButton", G2L["2"]);
+G2L["c"]["TextWrapped"] = true;
+G2L["c"]["BorderSizePixel"] = 0;
+G2L["c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["c"]["TextSize"] = 14;
+G2L["c"]["TextScaled"] = true;
+G2L["c"]["BackgroundColor3"] = Color3.fromRGB(101, 101, 101);
+G2L["c"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["c"]["Size"] = UDim2.new(0.12923, 0, 0.11402, 0);
+G2L["c"]["Name"] = [[ExSerNew]];
+G2L["c"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["c"]["Text"] = [[ExSerNew]];
+G2L["c"]["Position"] = UDim2.new(0.86073, 0, 0.26288, 0);
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.ExSerNew.LocalScript
+G2L["d"] = Instance.new("LocalScript", G2L["c"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.K00pgui v11
+G2L["e"] = Instance.new("TextButton", G2L["2"]);
+G2L["e"]["TextWrapped"] = true;
+G2L["e"]["BorderSizePixel"] = 0;
+G2L["e"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["e"]["TextSize"] = 14;
+G2L["e"]["TextScaled"] = true;
+G2L["e"]["BackgroundColor3"] = Color3.fromRGB(101, 101, 101);
+G2L["e"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["e"]["Size"] = UDim2.new(0.12923, 0, 0.11402, 0);
+G2L["e"]["Name"] = [[K00pgui v11]];
+G2L["e"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["e"]["Text"] = [[infinite yield SS]];
+G2L["e"]["Position"] = UDim2.new(0.86073, 0, 0.40858, 0);
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.K00pgui v11.LocalScript
+G2L["f"] = Instance.new("LocalScript", G2L["e"]);
+
+
+
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.LocalScript
+local function C_4()
+local script = G2L["4"];
+	script.Parent.Draggable = true
+	script.Parent.Active = true
+end;
+task.spawn(C_4);
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.infinite yield SS.LocalScript
+local function C_7()
+local script = G2L["7"];
+	local button = script.Parent
+	local textBox = button.Parent:FindFirstChild("TextBox")
+	
+	-- Lấy tên người chơi
+	local player = game.Players.LocalPlayer
+	local username = player and player.Name or "Unknown"
+	
+	if textBox then
+		button.MouseButton1Click:Connect(function()
+			textBox.Text = 'require(7634392335)("' .. username .. '")'
+		end)
+	end
+end;
+task.spawn(C_7);
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Clear.LocalScript
+local function C_9()
+local script = G2L["9"];
+	local button = script.Parent
+	local textBox = button.Parent:FindFirstChild("TextBox")
+	
+	if textBox then
+		button.MouseButton1Click:Connect(function()
+			textBox.Text = ""
+		end)
+	end
+end;
+task.spawn(C_9);
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.Executor.LocalScript
+local function C_b()
+local script = G2L["b"];
+	local button = script.Parent
+	local textBox = button.Parent:FindFirstChild("TextBox")
+	local remote = game.ReplicatedStorage:FindFirstChild("CommandRemote")
+	
+	local player = game.Players.LocalPlayer
+	local username = player and player.Name or "Unknown"
+	
+	local currentModule = 1
+	local modules = {
+		'require(7634392335)("' .. username .. '")',
+		'require(10868847330):pls("' .. username .. '")',
+		'require(15267263357).V11("' .. username .. '")'
+	}
+	
+	if textBox then
+		button.MouseButton1Click:Connect(function()
+			local command = textBox.Text
+	
+			if command == modules[1] then
+				if remote then
+					remote:FireServer("Module1", username)
+				end
+			elseif command == modules[2] then
+				if remote then
+					remote:FireServer("Module2", username)
+				end
+			elseif command == modules[3] then
+				if remote then
+					remote:FireServer("Module3", username)
+				end
+			else
+				textBox.Text = "LỆNH KHÔNG HỢP LỆ!"
+			end
+		end)
+	end
+	
+	local switchButton = button.Parent:FindFirstChild("Switch")
+	if switchButton then
+		switchButton.MouseButton1Click:Connect(function()
+			currentModule = currentModule % #modules + 1
+			textBox.Text = modules[currentModule]
+		end)
+	end
+end;
+task.spawn(C_b);
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.ExSerNew.LocalScript
+local function C_d()
+local script = G2L["d"];
+	local button = script.Parent
+	local textBox = button.Parent:FindFirstChild("TextBox")
+	
+	-- Lấy tên người chơi
+	local player = game.Players.LocalPlayer
+	local username = player and player.Name or "Unknown"
+	
+	if textBox then
+		button.MouseButton1Click:Connect(function()
+			textBox.Text = 'require(10868847330):pls("' .. username .. '")'
+		end)
+	end
+end;
+task.spawn(C_d);
+-- StarterGui.5C7835335C7837305C7837395C7834335C7835305C783539.Main.K00pgui v11.LocalScript
+local function C_f()
+local script = G2L["f"];
+	local button = script.Parent
+	local textBox = button.Parent:FindFirstChild("TextBox")
+	
+	-- Lấy tên người chơi
+	local player = game.Players.LocalPlayer
+	local username = player and player.Name or "Unknown"
+	
+	if textBox then
+		button.MouseButton1Click:Connect(function()
+			textBox.Text = 'require(15267263357).V11("' .. username .. '")'
+		end)
+	end
+end;
+task.spawn(C_f);
+
+return G2L["1"], require;
